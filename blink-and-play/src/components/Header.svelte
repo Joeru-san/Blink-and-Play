47586f2d4d7bbd0400f1c7af2488334d173d1e33
illmattep <kippermint6@gmail.com>
2023-12-28 15:20:48 +0100

    back button feature implemented

--- blink-and-play/src/components/Header.svelte
+++ blink-and-play/src/components/Header.svelte
@@ -1,6 +1,6 @@
 <header class="sticky px-6 py-6 duration-200 flex items-center justify-evenly">
     <div class="relative flex">
-        <button class="relative flex w-24 fa fa-arrow-circle-left fa-2x" aria-hidden="true"></button>
+        <button class="relative flex w-24 fa fa-arrow-circle-left fa-2x" aria-hidden="true" onclick="window.location.reload();"></button>
     </div>
 
     <div class="relative flex">
