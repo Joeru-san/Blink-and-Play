d8862d33c8e65594dc2460e2816ed87d08ee6046
Joeru-san <gioxel.vicario@gmail.com>
2024-01-02 18:52:41 +0100

    Refactor Main.svelte component layout

--- blink-and-play/src/components/Main.svelte
+++ blink-and-play/src/components/Main.svelte
@@ -14,31 +14,31 @@
     onMount(() => {
       fadeIn = true;
     });
-  </script>
+</script>
   {#if fadeIn}
-  <main transition:fade={{ duration: 1000 }} class="relative flex flex-row top-10 justify-center">
-      <section class="justify-evenly max-w-full grid grid-cols-1 lg:grid-cols-3 gap-24 lg:gap-52">
-        <Cards
-          imgSrc="/src/assets/core-actions/actions.png"
-          imgAlt="Actions category"
-          imgHeader="Actions"
-          cardAction={() => handleCardClick("actions")}
-        />
-  
-        <Cards
-          imgSrc="/src/assets/core-actions/drinks.png"
-          imgAlt="Drinks category"
-          imgHeader="Drinks"
-          cardAction={() => handleCardClick("drinks")}
-        />
-  
-        <Cards
-          imgSrc="/src/assets/core-actions/foods.png"
-          imgAlt="Foods category"
-          imgHeader="Foods"
-          cardAction={() => handleCardClick("foods")}
-        />
-      </section>
-  </main>
+    <main transition:fade={{ duration: 1000 }} class="relative flex flex-row top-10 justify-center">
+        <section class="justify-evenly max-w-full grid grid-cols-1 lg:grid-cols-3 gap-24 lg:gap-52">
+            <Cards
+              imgSrc="/src/assets/core-actions/actions.png"
+              imgAlt="Actions category"
+              imgHeader="Actions"
+              cardAction={() => handleCardClick("actions")}
+            />
+      
+            <Cards
+              imgSrc="/src/assets/core-actions/drinks.png"
+              imgAlt="Drinks category"
+              imgHeader="Drinks"
+              cardAction={() => handleCardClick("drinks")}
+            />
+      
+            <Cards
+              imgSrc="/src/assets/core-actions/foods.png"
+              imgAlt="Foods category"
+              imgHeader="Foods"
+              cardAction={() => handleCardClick("foods")}
+            />
+        </section>
+    </main>
   {/if}
   
