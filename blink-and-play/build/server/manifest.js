f2afdbba475f4e912332c3242fe1cc5f9868240b
Joeru-san <gioxel.vicario@gmail.com>
2024-01-02 19:55:47 +0100

    Update favicon in build and version number

--- blink-and-play/build/server/manifest.js
+++ blink-and-play/build/server/manifest.js
@@ -10,12 +10,12 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.I3tA25bH.js","app":"_app/immutable/entry/app.Sj5Ggpxu.js","imports":["_app/immutable/entry/start.I3tA25bH.js","_app/immutable/chunks/scheduler.DBUx310a.js","_app/immutable/chunks/singletons.P8TqTLp6.js","_app/immutable/entry/app.Sj5Ggpxu.js","_app/immutable/chunks/preload-helper.0HuHagjb.js","_app/immutable/chunks/scheduler.DBUx310a.js","_app/immutable/chunks/index.Zxmpjznd.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.fwJH7_aO.js","app":"_app/immutable/entry/app.Yij5-OeA.js","imports":["_app/immutable/entry/start.fwJH7_aO.js","_app/immutable/chunks/scheduler.DBUx310a.js","_app/immutable/chunks/singletons.66jiVEz1.js","_app/immutable/entry/app.Yij5-OeA.js","_app/immutable/chunks/preload-helper.0HuHagjb.js","_app/immutable/chunks/scheduler.DBUx310a.js","_app/immutable/chunks/index.WVKZ9uOM.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
-			__memo(() => import('./chunks/0-3yzsIlD-.js')),
-			__memo(() => import('./chunks/1-Pk-ifRBN.js')),
-			__memo(() => import('./chunks/2-Mbd1mV-y.js')),
-			__memo(() => import('./chunks/3-obWoe9B0.js'))
+			__memo(() => import('./chunks/0-jQmZ4Cgx.js')),
+			__memo(() => import('./chunks/1-SJ1vpQKN.js')),
+			__memo(() => import('./chunks/2-87wJ0sCt.js')),
+			__memo(() => import('./chunks/3-6vCm3m7n.js'))
 		],
 		routes: [
 			{
